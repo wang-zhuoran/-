877f520e18b23103cd77266da62e1ed7b7dfb7b0
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-04-24 15:10:05 +0800

    Create mql_squares.py

---
+++ 绘制简单折线图/mql_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+squares = [0, 1, 4, 9, 16, 25, 36]
+
+plt.plot(squares, linewidth=5)
+
+plt.title("Square Numbers", fontsize = 24)
+
+plt.xlabel("Value", fontsize=14)
+
+plt.ylabel("Square of value", fontsize=14)
+
+plt.tick_params(axis='both', labelsize=14)
+
+plt.show()
+
