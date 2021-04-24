c4c70c2d48221faba34df6ee27bc0b8cb458b57f
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-04-24 15:12:09 +0800

    Update mql_squares.py

--- 绘制简单折线图/mql_squares.py
+++ 绘制简单折线图/mql_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [0, 1, 2, 3, 4, 5, 6]
 squares = [0, 1, 4, 9, 16, 25, 36]
 
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 plt.title("Square Numbers", fontsize = 24)
 
